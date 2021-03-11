d79a191426e9c09a20ec153cb55d0183eafe507f
Mical15 <Michael.Gallien2@gmail.com>
2021-03-10 20:58:02 -0800

    Created Cookie

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,7 +1,36 @@
 const models = require('../models');
 const Promise = require('bluebird');
+const parsedCookie = require('./middleware');
 
 module.exports.createSession = (req, res, next) => {
+  // if no cookies => create hash => create session => create cookie => send cookie in response
+  // if Cookie is present => get hash => check if hash is the same as stored hash
+  // if cookie is not valid, make cookie/session/send cookie
+
+
+  if (!req.cookies) {
+    models.Sessions.create();
+    // .then((sessiondata) => {
+    //   req.session = sessiondata;
+    //   res.cookie = ('shortlyid=' + req.session.hash);
+    //   // res.cookie(name, value [,options]);
+    //   })
+    // .then( ()=> {
+
+    // })
+
+
+
+  } else {
+
+  }
+  var cookie = parsedCookie;
+  var session = {}; /// index username hash
+
+
+
+
+
 };
 
 /************************************************************/
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -11,23 +11,6 @@ const parseCookies = (req, res, next) => {
 
   req.cookies = cookiesObj;
   next();
-  // // console.log(req.headers);
-  // if (!req.headers.cookie) {
-  //   req.cookies = {};
-  // } else {
-  //   var cookieArray = req.headers.cookie.split(';');
-  //   var paresdCookies = cookieArray.reduce((result, cookie) => {
-  //     var cookie = cookie.split('=');
-  //     // console.log('inside of parsedcookies' + cookie);
-
-  //     result[cookie[0]] = cookie[1];
-  //     return result;
-  //   }, {});
-  //   req.cookies = paresdCookies;
-  // }
-
-  // console.log(req.cookies);
-  // next();
 };
 
 module.exports = parseCookies;
