0228c32b0d27d6884a68e4af862510e806f7d403
Mical15 <Michael.Gallien2@gmail.com>
2021-03-11 19:33:24 -0800

    Complete Createsession Middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,36 +1,62 @@
 const models = require('../models');
 const Promise = require('bluebird');
-const parsedCookie = require('./middleware');
+// const parsedCookie = require('./middleware');
 
 module.exports.createSession = (req, res, next) => {
-  // if no cookies => create hash => create session => create cookie => send cookie in response
-  // if Cookie is present => get hash => check if hash is the same as stored hash
-  // if cookie is not valid, make cookie/session/send cookie
-
-
-  if (!req.cookies) {
-    models.Sessions.create();
-    // .then((sessiondata) => {
-    //   req.session = sessiondata;
-    //   res.cookie = ('shortlyid=' + req.session.hash);
-    //   // res.cookie(name, value [,options]);
-    //   })
-    // .then( ()=> {
-
-    // })
-
-
-
+  if (!req.cookies.shortlyid) {
+    models.Sessions.create()
+      .then((result) => {
+        var id = result.insertId;
+        return models.Sessions.get({ id });
+      })
+      .then((sessionData) => {
+        req.session = sessionData;
+        res.cookie('shortlyid', req.session.hash);
+      })
+      .then(() => {
+        var username = req.body.username;
+        req.session.user = { username };
+        return models.Users.get({ username });
+      })
+      .then((userData) => {
+        if (userData && userData.id) {
+          req.Session.userId = userData.id;
+        }
+        next();
+      });
   } else {
+    var hash = req.cookies.shortlyid;
+    return models.Sessions.get({ hash })
+      .then((sessionData) => {
+        if (sessionData) {
+          req.session = sessionData;
+          res.cookie = ('shortlyid', req.session.hash);
+          var id = sessionData.userId;
+          if (id) {
+            id = req.session.userId;
+            return models.Users.get({ id })
+              .then((userData) => {
+                req.session.user = { username: userData.username };
+                next();
+              });
+          } else {
+            next();
+          }
 
+        } else {
+          models.Sessions.create()
+            .then((result) => {
+              var id = result.insertId;
+              return models.Sessions.get({ id });
+            })
+            .then((sessionData) => {
+              req.session = sessionData;
+              res.cookie('shortlyid', req.session.hash);
+              next();
+            });
+        }
+      });
   }
-  var cookie = parsedCookie;
-  var session = {}; /// index username hash
-
-
-
-
-
 };
 
 /************************************************************/
